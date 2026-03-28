16287602164084ca4ec3423542036ce2bdb9e1c3
radla <radlaoliveira5@gmail.com>
2026-03-28 11:33:52 -0300

    Atividade 0 LP1 em  C++ concluida

---
+++ LP1/aula 0/gestao_escolar.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+#define MAX_ALUNOS 50
+
+class Data {
+    public:
+    int dia;
+    int mes;
+    int ano;
+
+    void imprimirData(){
+        cout << dia << "/" << mes << "/" << ano << endl;
+    }
+};
+
+class Aluno{
+    public:
+    int matricula;
+    string nome;
+    Data nascimento;
+
+    void imprimirAluno(){
+        cout << "Matricula: " << matricula << " | Nome: " << nome << " | Nasc: ";
+        nascimento.imprimirData(); 
+    }
+};
+
+int main(){
+    Aluno turma[MAX_ALUNOS] ;
+    int totalAlunos = 0;
+    int opcao =-1;
+
+    cout << "=== Sistema de Gestao Escolar ===" << endl;
+
+    while(opcao != 0){
+        cout << "Menu:" << endl;
+        cout << "1. Adicionar Aluno" << endl;
+        cout << "2. Listar Alunos" << endl;
+        cout << "0. Sair" << endl;
+        cout << "Escolha: " <<endl;
+        cin >> opcao;
+
+        if (opcao == 1){
+            if (totalAlunos < MAX_ALUNOS){
+                cout << "Digite a matricula: "<< endl;
+                cin >> turma[totalAlunos].matricula;
+
+                cout << "Digite o primeiro nome do aluno: " << endl;
+                cin >> turma[totalAlunos].nome;
+
+                cout << "Data de nascimento (Dia): "<< endl;
+                cin >> turma[totalAlunos].nascimento.dia;
+                cout << "Data de nascimento (Mes): " << endl;
+                cin >> turma[totalAlunos].nascimento.mes;
+                cout << "Data de nascimento (Ano): " << endl;
+                cin >> turma[totalAlunos].nascimento.ano;
+
+                totalAlunos++;
+                cout << "Aluno adicionado com sucesso!" << endl;
+            } else{
+                cout <<"Capacidade maxima da turma atingida!" << endl;
+            }
+        } else if (opcao==2){
+            cout << "--- Lista de Alunos ---" << endl;
+
+            for (int i = 0; i < totalAlunos; i++)
+            {
+                turma[i].imprimirAluno();
+            }
+        } else if (opcao != 0){
+            cout << "Opcao invalida!" << endl;
+        }
+    }
+    return 0;
+}
