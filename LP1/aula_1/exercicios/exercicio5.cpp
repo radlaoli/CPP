3d3d3c12f1e7671e0b3dd132b1a4871512761d0d
radla <radlaoliveira5@gmail.com>
2026-04-18 20:03:36 -0300

    teste com entrada exercicio5 (aula1)

--- LP1/aula_1/exercicios/exercicio5.cpp
+++ LP1/aula_1/exercicios/exercicio5.cpp
@@ -2,36 +2,49 @@
 #include <string>
 using namespace std;
 
-class Produto{
-    public:
-        string nome;
-        float preco;
-        int quantidade;
+class Produto
+{
+public:
+    string nome;
+    float preco;
+    int quantidade;
 
-        void exibirInfo(){
-            cout << "Nome do produto: " << this->nome;
-            cout << " Preco: " << this->preco;
-            cout << " Quantidade: " << this->quantidade << endl;
-        }
+    void exibirInfo()
+    {
+        cout << "Nome do produto: " << this->nome;
+        cout << " Preco: " << this->preco;
+        cout << " Quantidade: " << this->quantidade << endl;
+    }
 };
-int main(){
+
+int main()
+{
 
     Produto prod[5];
     int expensiveId = 0;
     float valorEstoque = 0;
 
-    prod[0] = {"Monitor", 1200.0, 10};
-    prod[1] = {"Teclado", 250.0, 20};
-    prod[2] = {"Mouse", 150.0, 15};
-    prod[3] = {"Headset", 400.0, 8};
-    prod[4] = {"Mousepad", 80.0, 30};
-
-    for(int i = 0; i< 5; i++){
-        valorEstoque+= prod[i].preco;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> prod[i].nome;
+        cin >> prod[i].preco;
+        cin >> prod[i].quantidade;
+    } /*
+     prod[0] = {"Monitor", 1200.0, 10};
+     prod[1] = {"Teclado", 250.0, 20};
+     prod[2] = {"Mouse", 150.0, 15};
+     prod[3] = {"Headset", 400.0, 8};
+     prod[4] = {"Mousepad", 80.0, 30};
+  */
+    for (int i = 0; i < 5; i++)
+    {
+        valorEstoque += prod[i].preco;
     }
 
-    for(int i= 1; i<5; i++){
-        if(prod[i].preco > prod[expensiveId].preco){
+    for (int i = 1; i < 5; i++)
+    {
+        if (prod[i].preco > prod[expensiveId].preco)
+        {
             expensiveId = i;
         }
     }
